50cdb894c2bab177dc2682c5cdf6364f3cb9e937
Lenderson2 <lenderson2@users.noreply.github.com>
2016-07-30 18:58:35 -0700

    completed working prototype

--- logProcessor.py
+++ logProcessor.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import ipdb
 
 
 
@@ -11,37 +12,48 @@ class logProcessor(object):
 		"""
 		initialize empty logProcessor
 		"""
-		pass
+		self.time_sessions = None
+		self.averages = None
 
-	def process(self):
+	def process(self, filename):
 		"""
+		top level function to process log file
 		"""
-		pass
+		file = open(filename, 'r')
+		self.time_sessions = reduce(self.collect_time_sessions, file, {})
+		self.averages = {key: float(value[0])/value[1] \
+			for key, value in self.time_sessions.iteritems()}
+		file.close()
 
-	def total_time_sessions(self):
+	def collect_time_sessions(self, collection, record):
 		"""
-		"""
-		pass
+		reduce function for use in self.process
 
-	def average(self):
+		returns dict "collection" for continued reduction
 		"""
-		"""
-		pass
+		r = record.strip().split(',')
+		userid, time, action = r[0], int(r[1]), r[2]
+		if userid not in collection:
+			collection[userid] = [0, 0, time, action]
+			return collection
+		last_record = collection[userid]
+		if last_record[3] == 'open' and action == 'close':
+			tot_time = last_record[0] + time - last_record[2]
+			sessions = collection[userid][1] + 1
+			update = [tot_time, sessions, time, action]
+			collection[userid] = update
+			return collection
+		else:
+			collection[userid][2] = time
+			collection[userid][3] = action
+			return collection
 
 	def to_csv(self):
 		"""
 		"""
 		pass
 
-	def to_list(self):
-		"""
-		"""
-		pass
-
-	def read_file(self):
-		"""
-		"""
-		pass
-
-
-
+if __name__ == '__main__':
+	lp = logProcessor()
+	lp.process('test.log')
+	print lp.averages
